7fadbfb3bbbee314a550e10845f9a77891520720
Duncan Macleod <duncan.macleod@ligo.org>
2021-04-14 11:50:42 +0100

    docs: move to calver versioning
    using YYYY.0M.0D, see https://calver.org/#scheme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,8 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import datetime
+
 master_doc = 'index'
 
 # -- Path setup --------------------------------------------------------------
@@ -24,7 +26,8 @@ copyright = '2021, Jose Munoz'
 author = 'Jose Munoz'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+calver_scheme = "%Y.%m.%d"
+release = datetime.datetime.utcnow().strftime(calver_scheme)
 
 
 # -- General configuration ---------------------------------------------------
